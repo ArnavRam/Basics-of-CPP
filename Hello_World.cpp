627ae41b9e842ab2d2f4fcf34a5c144fdbbe3c4c
Arnav Ram <169087658+ArnavRam@users.noreply.github.com>
2025-08-24 19:46:26 +0530

    Add files via upload

---
+++ Hello_World.cpp
@@ -0,0 +1,13 @@
+// NAME: Arnav Ram
+// PRN: 25070123503
+// Exp-1
+// Program-1 Hello World
+#include<iostream>
+using namespace std;
+int main() {
+    cout << "Hello World" << endl;
+    return 0;
+}
+
+/*OUTPUT
+Hello World*/
