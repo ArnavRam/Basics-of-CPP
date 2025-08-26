363c9c8f6b12955ff11854891c9c7f9a073814cf
Arnav Ram <169087658+ArnavRam@users.noreply.github.com>
2025-08-26 16:04:41 +0530

    Add files via upload

---
+++ Basic_Calculator.cpp
@@ -0,0 +1,25 @@
+// NAME: Arnav Ram
+// PRN: 25070123503
+// Exp-1
+// Program-2: Basic Calculator
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, sum;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+    sum = num1 + num2;
+    cout << "Sum of given numbers = " << sum << endl;
+    
+    return 0;
+}
+
+/*OUTPUT
+Enter first number: 4
+Enter second number: 8
+Sum of given numbers = 12
+*/
